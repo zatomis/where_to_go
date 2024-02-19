4ff2d38332b54a53b941ace76a4a161856ecf6e0
zatomis <zatomis@list.ru>
2024-02-19 14:55:14 +0300

    Коррекция кода

--- places/views.py
+++ places/views.py
@@ -12,18 +12,6 @@ def show_main(request):
         main_url = request.build_absolute_uri()
         path_url = reverse("places", args=(place.pk,))
         pictures = [pic.image.url for pic in place.images.all()]
-
-        # place_details = {
-        #     "title": place.title,
-        #     "description_short": place.short_description,
-        #     "description_long": place.long_description,
-        #     "coordinates": {
-        #         "lng": place.lon,
-        #         "lat": place.lat,
-        #     },
-        #     "imgs": pictures,
-        # }
-
         place_geodata = {
             "type": "Feature",
             "geometry": {
@@ -37,7 +25,6 @@ def show_main(request):
             }
         }
         features.append(place_geodata)
-
     places_geojson = {
       "type": "FeatureCollection",
       "features": features,
